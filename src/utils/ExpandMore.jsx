507509b64fc5e5c085f3a8bfc16760e1c6fae52b
TimIsOverpowered <admin@angelthump.com>
2026-03-05 23:58:31 -0600

    fix warning

--- src/utils/ExpandMore.jsx
+++ src/utils/ExpandMore.jsx
@@ -1,18 +1,13 @@
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 
-const ExpandMore = styled(({ ...props }) => <IconButton {...props} />)`
+const ExpandMore = styled(IconButton, {
+  shouldForwardProp: (prop) => prop !== 'expand',
+})`
   margin-left: auto;
   transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 
-  ${(props) =>
-    props.expand
-      ? `
-        transform: rotate(0deg);
-      `
-      : `
-        transform: rotate(180deg);
-      `}
+  ${({ expand }) => (expand ? `transform: rotate(0deg);` : `transform: rotate(180deg);`)}
 `;
 
 export default ExpandMore;
