d684a9cf328ecd7bf3ebebb6a0ee0cd54347a4aa
TimIsOverpowered <admin@angelthump.com>
2026-03-05 23:50:10 -0600

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,72 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+    dts({
+      include: ['src/**/*'],
+      outDir: 'dist',
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
       name: 'VodComponents',
-      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', '@mui/material', '@mui/icons-material', 'video.js', 'simplebar-react', 'dayjs', 'tinyduration', 'react-router-dom', '@mui/x-date-pickers', 'react-twemoji', '@feathersjs/feathers', '@feathersjs/rest-client', '@emotion/react', '@emotion/styled', 'prop-types'],
+      external: (id) => {
+        const externals = [
+          'react',
+          'react-dom',
+          'react/jsx-runtime',
+          'react/jsx-dev-runtime',
+          '@mui/material',
+          '@mui/icons-material',
+          '@mui/system',
+          '@mui/utils',
+          'video.js',
+          'simplebar-react',
+          'dayjs',
+          'tinyduration',
+          'react-router-dom',
+          '@mui/x-date-pickers',
+          'react-twemoji',
+          '@feathersjs/feathers',
+          '@feathersjs/rest-client',
+          '@emotion/react',
+          '@emotion/styled',
+          'prop-types',
+        ];
+        return externals.some((pkg) => id === pkg || id.startsWith(pkg + '/'));
+      },
       output: {
         globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        '@mui/material': '@mui/material',
-        '@mui/icons-material': '@mui/icons-material',
-        'video.js': 'videojs',
-        'simplebar-react': 'SimpleBar',
-        dayjs: 'dayjs',
-        tinyduration: 'tinyduration',
-        'react-router-dom': 'ReactRouterDOM',
-        '@mui/x-date-pickers': '@mui/x-date-pickers',
-        'react-twemoji': 'ReactTwemoji',
-        '@feathersjs/feathers': 'FeathersJS',
-        '@feathersjs/rest-client': 'RestClient',
-        '@emotion/react': 'EmotionReact',
-        '@emotion/styled': 'EmotionStyled',
-        'prop-types': 'PropTypes'
-      }
-      }
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'ReactJsxRuntime',
+          '@mui/material': '@mui/material',
+          '@mui/icons-material': '@mui/icons-material',
+          'video.js': 'videojs',
+          'simplebar-react': 'SimpleBar',
+          dayjs: 'dayjs',
+          tinyduration: 'tinyduration',
+          'react-router-dom': 'ReactRouterDOM',
+          '@mui/x-date-pickers': '@mui/x-date-pickers',
+          'react-twemoji': 'ReactTwemoji',
+          '@feathersjs/feathers': 'FeathersJS',
+          '@feathersjs/rest-client': 'RestClient',
+          '@emotion/react': 'EmotionReact',
+          '@emotion/styled': 'EmotionStyled',
+          'prop-types': 'PropTypes',
+        },
+      },
     },
-    sourcemap: true
+    sourcemap: true,
   },
-  plugins: [
-    dts({
-      include: ['src/**/*'],
-      outDir: 'dist'
-    })
-  ]
 });
